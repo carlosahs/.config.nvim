a0f04383270649c52d8e7bcc6f7602bdddb2f266
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2025-02-15 16:56:52 -0600

    feat: update window key mappings

--- init.lua
+++ init.lua
@@ -25,10 +25,7 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.g.have_nerd_font = false
 
-vim.keymap.set("n", "<leader>j/", "<c-w>|", { silent = true })
 vim.keymap.set("n", "<leader>j", "<c-w>", { silent = true })
-vim.keymap.set("n", "<leader>j-", "<c-w>s", { silent = true })
-vim.keymap.set("n", "<leader>j|", "<c-w>v", { silent = true })
 
 vim.keymap.set("n", "<leader>k", "<cmd>tabnew<CR>", { silent = true })
 
