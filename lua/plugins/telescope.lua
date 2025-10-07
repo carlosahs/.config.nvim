8ba4ea0fd214762119199551cbf2f72c37d41e1a
Carlos Hurtado <cayalber@amazon.com>
2025-10-07 12:05:45 -0600

    feat(telescope): add git branch search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -31,6 +31,7 @@ return {
     pcall(require('telescope').load_extension, 'fzf')
     pcall(require('telescope').load_extension, 'ui-select')
     local builtin = require 'telescope.builtin'
+    local themes = require 'telescope.themes'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
@@ -42,7 +43,7 @@ return {
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
     vim.keymap.set('n', '<leader>/', function()
-      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+      builtin.current_buffer_fuzzy_find(themes.get_dropdown {
         winblend = 10,
         previewer = false,
       })
@@ -56,6 +57,12 @@ return {
     vim.keymap.set('n', '<leader>sn', function()
       builtin.find_files { cwd = vim.fn.stdpath 'config' }
     end, { desc = '[S]earch [N]eovim files' })
+    vim.keymap.set('n', '<leader>gb', function()
+      builtin.git_branches(themes.get_dropdown {
+        winblend = 10,
+        previewer = false,
+      })
+    end, { desc = '[G]it [B]ranches search'})
   end
 }
 
