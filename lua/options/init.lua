dda6cd6ebc11b525da467949c645b4722097774c
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2025-10-12 18:57:19 -0600

    chore: add options Lua module (#47)
    Co-authored-by: Carlos Hurtado <cayalber@amazon.com>

---
+++ lua/options/init.lua
@@ -0,0 +1,10 @@
+vim.o.mouse = ""
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.clipboard = "unnamedplus"
+vim.o.cursorline = true
+vim.o.hlsearch = true
+vim.o.completeopt = "menu,menuone,noinsert"
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣", leadmultispace = "↦ " }
+
