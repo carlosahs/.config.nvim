7d6fbd3253831186ca84b4d72bbc1ca8c1394b6d
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2026-01-30 13:41:00 -0600

    feat(plugin): add Metals for Scala LSP support

---
+++ lua/plugins/metals.lua
@@ -0,0 +1,24 @@
+return {
+  "scalameta/nvim-metals",
+  ft = { "scala", "sbt", "java" },
+  opts = function()
+    local metals_config = require("metals").bare_config()
+    metals_config.on_attach = function(client, bufnr)
+      local opts = { buffer = bufnr, noremap = true, silent = true }
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    end
+    metals_config.find_root_dir_max_project_nesting = 8
+    return metals_config
+  end,
+  config = function(self, metals_config)
+    local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = self.ft,
+      callback = function()
+        require("metals").initialize_or_attach(metals_config)
+      end,
+      group = nvim_metals_group,
+    })
+  end
+}
