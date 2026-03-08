f68c3bc2e55e726d8c03cee3287910d217c5c3f1
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2026-03-08 16:06:51 -0600

    fix: inconsistent indentation

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,13 +3,13 @@ return {
   branch = 'master',
   lazy = false,
   build = ":TSUpdate",
-	ensure_installed = { "bash", "c", "html", "lua", "luadoc", "markdown", "vim", "vimdoc", "rust", "go", "css" },
-	auto_install = true,
-	hightlight = {
-		enable = true,
-		additional_vim_regex_highlighting = { "ruby" },
-	},
-	indent = { enable = true, disable = { "ruby", "html", "python", "rust", "typescript" }},
+  ensure_installed = { "bash", "c", "html", "lua", "luadoc", "markdown", "vim", "vimdoc", "rust", "go", "css" },
+  auto_install = true,
+  hightlight = {
+    enable = true,
+    additional_vim_regex_highlighting = { "ruby" },
+  },
+  indent = { enable = true, disable = { "ruby", "html", "python", "rust", "typescript" }},
   config = function(_, opts)
     require("nvim-treesitter.install").prefer_git = true
     require("nvim-treesitter.configs").setup(opts)
