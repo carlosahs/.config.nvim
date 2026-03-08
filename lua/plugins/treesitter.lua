e64e87809173f3f911cd494c5a9d2f1ae86159e1
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2026-03-08 15:59:38 -0600

    chore: update Tree-sitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
-local plugin = {"nvim-treesitter/nvim-treesitter"}
-plugin.build = ":TsUpdate"
-plugin.opts = {
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = 'master',
+  lazy = false,
+  build = ":TSUpdate",
 	ensure_installed = { "bash", "c", "html", "lua", "luadoc", "markdown", "vim", "vimdoc", "rust", "go", "css" },
 	auto_install = true,
 	hightlight = {
@@ -8,10 +10,9 @@ plugin.opts = {
 		additional_vim_regex_highlighting = { "ruby" },
 	},
 	indent = { enable = true, disable = { "ruby", "html", "python", "rust", "typescript" }},
+  config = function(_, opts)
+    require("nvim-treesitter.install").prefer_git = true
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
-plugin.config = function(_, opts)
-	require("nvim-treesitter.install").prefer_git = true
-	require("nvim-treesitter.configs").setup(opts)
-end
-return plugin
 
