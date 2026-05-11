8d46f2a5aec15802bb637a46bcaa30e1db15caa6
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2026-05-11 09:49:57 -0600

    chore: update terminal mappings

--- lua/keymaps/toggleterm/init.lua
+++ lua/keymaps/toggleterm/init.lua
@@ -2,17 +2,13 @@ local Terminal = require("toggleterm.terminal").Terminal
 local terminals = {
   lazygit = {},
   tmux = {},
+  copilot = {},
 }
 
 function _on_open(term)
-  vim.cmd("startinsert!")
   local keymap_opts = { noremap = true, silent = true, buffer = term.bufnr }
   vim.keymap.set("t", "<c-]>", "<c-\\><c-n>", keymap_opts)
   vim.keymap.set("n", "q", "<cmd>close<CR>", keymap_opts)
-end
-
-function _on_close(term)
-  vim.cmd("startinsert!")
 end
 
 function _lazygit_toggle()
@@ -23,26 +19,38 @@ function _lazygit_toggle()
       cmd = "lazygit",
       hidden = true,
       on_open = _on_open,
-      on_close = _on_close,
     })
     terminals.lazygit[cwd] = lazygit
   end
   lazygit:toggle()
 end
 
-function _tmux_cwd_toggle()
+function _tmux_toggle()
   local cwd = vim.fn.getcwd()
   local tmux = terminals.tmux[cwd]
   if tmux == nil then
     tmux = Terminal:new({
-      cmd = string.format("tmux new-session -A -s %s", cwd),
+      cmd = "tmux new-session -A -s nvim",
       hidden = true,
       on_open = _on_open,
-      on_close = _on_close,
     })
     terminals.tmux[cwd] = tmux
   end
   tmux:toggle()
+end
+
+function _copilot_toggle()
+  local cwd = vim.fn.getcwd()
+  local copilot = terminals.copilot[cwd]
+  if copilot == nil then
+    copilot = Terminal:new({
+      cmd = "copilot",
+      hidden = true,
+      on_open = _on_open,
+    })
+    terminals.copilot[cwd] = copilot
+  end
+  copilot:toggle()
 end
 
 vim.keymap.set(
@@ -54,7 +62,13 @@ vim.keymap.set(
 vim.keymap.set(
   "n",
   "<leader>lj",
-  "<cmd>lua _tmux_cwd_toggle()<CR>",
+  "<cmd>lua _tmux_toggle()<CR>",
+  { noremap = true, silent = true }
+)
+vim.keymap.set(
+  "n",
+  "<leader>lk",
+  "<cmd>lua _copilot_toggle()<CR>",
   { noremap = true, silent = true }
 )
 
