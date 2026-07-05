57b79fea08959cae603830be47529a3527959809
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2026-07-05 17:40:34 -0600

    chore: update Telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -32,37 +32,37 @@ return {
     pcall(require('telescope').load_extension, 'ui-select')
     local builtin = require 'telescope.builtin'
     local themes = require 'telescope.themes'
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+    vim.keymap.set('n', '<leader>sa', builtin.help_tags, { desc = '[S]earch [A] help tags' })
+    vim.keymap.set('n', '<leader>ss', builtin.keymaps, { desc = '[S]earch [S] keymaps' })
+    vim.keymap.set('n', '<leader>sd', builtin.find_files, { desc = '[S]earch [D] files' })
+    vim.keymap.set('n', '<leader>sf', builtin.builtin, { desc = '[S]earch [F] select Telescope' })
+    vim.keymap.set('n', '<leader>sq', builtin.live_grep, { desc = '[S]earch by [Q] grep' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+    vim.keymap.set('n', '<leader>se', builtin.diagnostics, { desc = '[S]earch [E] diagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-    vim.keymap.set('n', '<leader>/', function()
+    vim.keymap.set('n', '<leader>sz', builtin.oldfiles, { desc = '[S]earch [Z] old files' })
+    vim.keymap.set('n', '<leader>sx', builtin.buffers, { desc = '[S] Find existing [X] buffers' })
+    vim.keymap.set('n', '<leader>sc', function()
       builtin.current_buffer_fuzzy_find(themes.get_dropdown {
         winblend = 10,
         previewer = false,
       })
-    end, { desc = '[/] Fuzzily search in current buffer' })
-    vim.keymap.set('n', '<leader>s/', function()
+    end, { desc = 'Fuzzily [S]earch in [C]urrent buffer' })
+    vim.keymap.set('n', '<leader>sv', function()
       builtin.live_grep {
         grep_open_files = true,
         prompt_title = 'Live Grep in Open Files',
       }
-    end, { desc = '[S]earch [/] in Open Files' })
-    vim.keymap.set('n', '<leader>sn', function()
+    end, { desc = '[S] Live [V] grep' })
+    vim.keymap.set('n', '<leader>da', function()
       builtin.find_files { cwd = vim.fn.stdpath 'config' }
-    end, { desc = '[S]earch [N]eovim files' })
-    vim.keymap.set('n', '<leader>aa', function()
+    end, { desc = '[D] Search [A] config files' })
+    vim.keymap.set('n', '<leader>ds', function()
       builtin.git_branches(themes.get_dropdown {
         winblend = 10,
         previewer = false,
       })
-    end, { desc = '[aa] Git branches search' })
+    end, { desc = '[D] Git branches [S] dropdown' })
   end
 }
 
