8321c4ca7215c7e17894fcf83f6e58262851813d
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2026-08-11 22:35:29 -0600

    chore: reinstantiate explorer keymap

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,5 +1,6 @@
 require("keymaps.toggleterm")
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<leader>hh", "<cmd>Ex<CR>", { desc = "[H] Open [H] explorer"} )
 vim.keymap.set("n", "<leader>j", "<c-w>", { silent = true })
 
