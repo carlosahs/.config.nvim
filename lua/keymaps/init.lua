0242a77e26ed8f377d02a06e9cec78bf38ac5379
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2026-08-19 23:13:47 -0600

    chore: add tab keymaps

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -2,5 +2,8 @@ require("keymaps.toggleterm")
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<leader>hh", "<cmd>Ex<CR>", { desc = "[H] Open [H] explorer"} )
+vim.keymap.set("n", "<leader>kh", "gT", { desc = "[H] Go to [J] next [H] tab" })
+vim.keymap.set("n", "<leader>kj", "<cmd>tabnew<CR>", { desc = "[H] Open [J] new [J] tab" })
+vim.keymap.set("n", "<leader>kl", "gt", { desc = "[H] Go to [J] next [L] tab" })
 vim.keymap.set("n", "<leader>j", "<c-w>", { silent = true })
 
